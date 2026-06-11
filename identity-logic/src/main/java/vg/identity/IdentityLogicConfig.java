223bdb4550f9b243d5ffe157e50223dea1de252c
Vitaliy Gutarin <vgutarin@tango.me>
2026-06-11 19:33:24 +0300

    delegating encoder

--- identity-logic/src/main/java/vg/identity/IdentityLogicConfig.java
+++ identity-logic/src/main/java/vg/identity/IdentityLogicConfig.java
@@ -7,9 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+
+import java.util.Map;
 
 @Configuration
 @ComponentScan
@@ -20,7 +23,13 @@ public class IdentityLogicConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+        var defaultId = "argon2";
+        return new DelegatingPasswordEncoder(
+                defaultId,
+                Map.of(
+                        defaultId, Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()
+                )
+        );
     }
 
 
